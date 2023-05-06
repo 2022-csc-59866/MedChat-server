eaff6bb0d39d16465abc7168ec48c18471e11b04
Shahjalal Sikder <shah.sikder55@gmail.com>
2023-05-05 21:54:33 -0400

    working on server-client communication

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,13 @@
 const { connect } = require('getstream');
 const bcrypt = require('bcrypt');
-const StreamChat = require('stream-chat');
+const StreamChat = require('stream-chat').StreamChat;
 const crypto = require('crypto');
+
+require('dotenv').config();
 
 const api_key = process.env.STREAM_API_KEY;
 const api_secret = process.env.STREAM_API_SECRET;
-const api_id = process.env.STREAM_API_ID;
+const app_id = process.env.STREAM_APP_ID;
 
 const login = async (req, res) => {
     try {
